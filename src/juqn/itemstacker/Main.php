c8add5cbc5b13d930789cae9eef1141288e39949
JkqzDev <espulefjuan13@gmail.com>
2023-03-19 17:29:49 -0300

    Initial commit

---
+++ src/juqn/itemstacker/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace juqn\itemstacker;
+
+use pocketmine\entity\object\ItemEntity;
+use pocketmine\event\entity\EntitySpawnEvent;
+use pocketmine\event\EventPriority;
+use pocketmine\plugin\PluginBase;
+
+final class Main extends PluginBase {
+
+    protected function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvent(EntitySpawnEvent::class, function (EntitySpawnEvent $event): void {
+            $entity = $event->getEntity();
+
+            if (!$entity instanceof ItemEntity) {
+                return;
+            }
+            $item = $entity->getItem();
+            $entities = $entity->getWorld()->getNearbyEntities($entity->getBoundingBox()->expand(5, 5, 5));
+
+            foreach ($entities as $en) {
+                if (!$en instanceof ItemEntity || $en->getId() === $entity->getId()) {
+                    continue;
+                }
+                $it = $en->getItem();
+
+                if ($it->getMaxStackSize() > 1 && $it->canStackWith($item)) {
+                    $en->flagForDespawn();
+                    $item->setCount($it->getCount() + $item->getCount());
+                }
+            }
+        }, EventPriority::NORMAL, $this);
+    }
+}
